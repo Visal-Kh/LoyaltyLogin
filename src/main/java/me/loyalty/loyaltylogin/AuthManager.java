7cf3dffa6f02fc4d1122dfb644df6101797aa47c
BroVisal <gfjhffb3@gmail.com>
2026-06-18 00:23:57 +0700

    Refactor password registration and validation methods

--- src/main/java/me/loyalty/loyaltylogin/AuthManager.java
+++ src/main/java/me/loyalty/loyaltylogin/AuthManager.java
@@ -5,22 +5,18 @@ import java.util.HashMap;
 import java.util.UUID;
 
 public class AuthManager {
-
     private final HashMap<UUID, String> registeredPasswords = new HashMap<>();
 
     public void register(UUID uuid, String password) {
-    // ប្រើ BCrypt.hashpw ដើម្បីលាក់ Password
-    String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt(12));
-    registeredPasswords.put(uuid, hashedPassword);
-}
+        String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt(12));
+        registeredPasswords.put(uuid, hashedPassword);
+    }
 
     public boolean checkPassword(UUID uuid, String password) {
-    if (!registeredPasswords.containsKey(uuid)) return false;
-    String hashedPassword = registeredPasswords.get(uuid);
-    // ប្រើ BCrypt.checkpw ដើម្បីផ្ទៀងផ្ទាត់
-    return BCrypt.checkpw(password, hashedPassword);
-}
-
+        if (!registeredPasswords.containsKey(uuid)) return false;
+        String hashedPassword = registeredPasswords.get(uuid);
+        return BCrypt.checkpw(password, hashedPassword);
+    }
 
     public boolean isRegistered(UUID uuid) {
         return registeredPasswords.containsKey(uuid);
