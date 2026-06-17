7177028ca4b0d2da429b1b591fe9f87d4855bc13
BroVisal <gfjhffb3@gmail.com>
2026-06-18 01:28:49 +0700

    Add login message for Premium Player and registration prompt

--- src/main/java/me/loyalty/loyaltylogin/LoginListener.java
+++ src/main/java/me/loyalty/loyaltylogin/LoginListener.java
@@ -1,1 +1,20 @@
+package me.loyalty.loyaltylogin;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class LoginListener implements Listener {
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        
+        // ពិនិត្យមើល Account Premium (ឈ្មោះបង)
+        if (player.getName().equals("Visal-Kh")) {
+            player.sendMessage("ស្វាគមន៍ Premium Player! បងត្រូវបាន Auto-Login រួចរាល់។");
+        } else {
+            player.sendMessage("សូមវាយ /register <password> ដើម្បីចុះឈ្មោះ។");
+        }
+    }
+}
